d3bbebcf5d02eb1f9cb67504ab0a6b18b55d7bfc
Sserunkuma Jonathan G. <jonathansserunkuma0@gmail.com>
2026-09-07 18:09:43 +0300

    chapter 1

--- Chapter_01/hello.cpp
+++ Chapter_01/hello.cpp
@@ -1,6 +1,7 @@
 // This program outputs the message "Hello, World!" to the monitor
 
-import std; // gain access to the C++ standard library
+//import std; // gain access to the C++ standard library
+#include <iostream> // gain access to the C++ standard library
 int main() // C++ programs start by executing the function main
 {
     std::cout << "Hello, World!\n"; // output "Hello, Wor ld!"
